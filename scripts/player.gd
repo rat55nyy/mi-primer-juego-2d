74c4a1fc9be669649c875d6599a1fea5521b18d6
rat55nyy <matiassaavedrallanca@gmail.com>
2025-10-12 23:39:51 -0300

    Clase 2

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,11 +1,18 @@
 extends Area2D
+signal hit 
 
 @export var speed = 400 
 var screen_size 
 
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
+
 
 func _ready() -> void:
 	screen_size = get_viewport_rect().size
+	hide()
 
 func _process(delta):
 	var velocity = Vector2.ZERO
@@ -23,6 +30,20 @@ func _process(delta):
 		$AnimatedSprite2D.play()
 	else:
 		$AnimatedSprite2D.stop()
+		
+	if velocity.x != 0:
+		$AnimatedSprite2D.animation = "walk"
+		$AnimatedSprite2D.flip_v = false
+		$AnimatedSprite2D.flip_h = velocity.x < 0
+	elif velocity.y != 0:
+		$AnimatedSprite2D.animation = "up"
+		$AnimatedSprite2D.flip_v = velocity.y > 0
 
 	position += velocity * delta
 	position = position.clamp(Vector2.ZERO, screen_size)
+
+
+func _on_body_entered(body: Node2D) -> void:
+	hide()
+	hit.emit()
+	$CollisionShape2D.set_deferred("disabled",true)
